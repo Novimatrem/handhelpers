996831ca2fa2c65dede5ad0b7039d8bf0be5d017
zoeyglobe <thenovimatrem@protonmail.ch>
2024-12-21 14:40:49 +0000

    Upload New File

---
+++ dosbox-launchers/dos-wolf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "DOSBox"
+echo "Copyright DOSBox Team, published under GNU GPL."
+echo "---"
+
+dosbox -c 'mount c /opt/DOSC' -c 'C:' -c 'cd WOLF3D' -c 'C:\WOLF3D\WOLF3D.EXE %1 %2 %3 %4 %5 %6 %7 %8 %9' -c 'exit' -noconfig -noautoexec > /dev/null
+
