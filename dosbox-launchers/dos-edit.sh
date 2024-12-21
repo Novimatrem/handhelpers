ab59dbfd7ccf81802ded689107b2a2a52d27be5d
Novimatrem <TheNovimatrem@protonmail.ch>
2024-12-21 16:53:51 +0000

    zoom

--- dosbox-launchers/dos-edit.sh
+++ dosbox-launchers/dos-edit.sh
@@ -4,5 +4,5 @@ echo "DOSBox"
 echo "Copyright DOSBox Team, published under GNU GPL."
 echo "---"
 
-dosbox -c 'mount c /opt/DOSC' -c 'mount x /' -c 'X:' -c 'cd home' -c 'cd zoey' -c 'C:\EDIT.EXE %1 %2 %3 %4 %5 %6 %7 %8 %9' -c 'exit' -noconfig -noautoexec > /dev/null
+dosbox -c 'mount c /opt/DOSC' -c 'mount x /' -c 'X:' -c 'cd home' -c 'cd zoey' -c 'cycles max' -c 'C:\EDIT.EXE %1 %2 %3 %4 %5 %6 %7 %8 %9' -c 'exit' -noconfig -noautoexec > /dev/null
 
--- dosbox-launchers/dos-wolf.sh
+++ dosbox-launchers/dos-wolf.sh
@@ -4,5 +4,5 @@ echo "DOSBox"
 echo "Copyright DOSBox Team, published under GNU GPL."
 echo "---"
 
-dosbox -c 'mount c /opt/DOSC' -c 'C:' -c 'cd WOLF3D' -c 'C:\WOLF3D\WOLF3D.EXE %1 %2 %3 %4 %5 %6 %7 %8 %9' -c 'exit' -noconfig -noautoexec > /dev/null
+dosbox -c 'mount c /opt/DOSC' -c 'C:' -c 'cd WOLF3D' -c 'cycles max' -c 'C:\WOLF3D\WOLF3D.EXE %1 %2 %3 %4 %5 %6 %7 %8 %9' -c 'exit' -noconfig -noautoexec > /dev/null
 
