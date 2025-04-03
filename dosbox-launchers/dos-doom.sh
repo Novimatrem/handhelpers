b1d2a173fd19e34a23b3b97490587df1e70cce03
Novimatrem <69379708+Novimatrem@users.noreply.github.com>
2025-04-03 03:07:18 +0100

    just ain't happening

--- dosbox-launchers/dos-doom.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "DOSBox"
-echo "Copyright DOSBox Team, published under GNU GPL."
-echo "---"
-# 21/Dec/2024 - Changed DOOM.EXE to FDOOM.EXE, FastDoom runs better
-dosbox -c 'mount c /opt/DOSC' -c 'C:' -c 'cd DOOM' -c 'cycles max' -c 'C:\DOOM\FDOOM.EXE %1 %2 %3 %4 %5 %6 %7 %8 %9' -c 'exit' -noconfig -noautoexec > /dev/null
-
